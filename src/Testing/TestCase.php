3b2c84da8247eb6175e450bcad3833a6e90b3d1d
Calvin Alkan <calvin@snicco.de>
2022-03-09 15:20:11 +0100

    use consistent test constants for codeception/phpunit

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -417,13 +417,13 @@ abstract class TestCase extends WPTestCase
             return;
         }
         
-        $url = $_ENV['APP_URL'] ?? 'https://localhost.test';
+        $url = $_ENV['SITE_URL'] ?? 'https://localhost.test';
         $this->config->set('app.url', $url);
         
         $request = new Request(
             $this->request_factory->createServerRequest(
                 'GET',
-                $this->createUri($_SERVER['FLUSHABLE_SITE_WP_URL'] ?? 'https://localhost.test'),
+                $this->createUri($_SERVER['SITE_URL'] ?? 'https://localhost.test'),
                 $this->default_server_variables
             )
         );
